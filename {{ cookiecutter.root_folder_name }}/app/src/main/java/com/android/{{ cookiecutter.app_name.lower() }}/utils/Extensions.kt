4f6e503d4b157b2f048c4395046336d11bcf2b60
Karan Nickelfox <karan@nickelfox.com>
2019-07-02 18:17:06 +0530

    fix(): modify email valid function

--- {{ cookiecutter.root_folder_name }}/app/src/main/java/com/android/{{ cookiecutter.app_name.lower() }}/utils/Extensions.kt
+++ {{ cookiecutter.root_folder_name }}/app/src/main/java/com/android/{{ cookiecutter.app_name.lower() }}/utils/Extensions.kt
@@ -3,4 +3,4 @@ package com.android.{{ cookiecutter.app_name.lower() }}.utils
 import android.util.Patterns
 
 val String.isNotValidEmail: Boolean
-    get() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    get() = !Patterns.EMAIL_ADDRESS.matcher(this).matches()
